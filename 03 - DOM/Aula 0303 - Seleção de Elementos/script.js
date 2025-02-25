cc3f586b2431f44f267f56e7af78e6275826dfec
Victor <victorgabrielhilario28@gmail.com>
2025-02-25 00:28:46 -0300

    Selectors

---
+++ 03 - DOM/Aula 0303 - Seleção de Elementos/script.js
@@ -0,0 +1,11 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[1]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
